6fac9b3d0f984b0f16ac814d8c5f298583dfcdcb
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-09-04 01:04:42 +0530

    Time: 27 ms (98.81%) | Memory: 16.3 MB (47.30%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,10 +1,5 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         
-        # return math.comb(m+n-2, m-1) 
-        @cache
-        def dfs(i,j):
-            if i==m-1 or j==n-1:
-                return 1
-            return dfs(i+1, j) + dfs(i, j+1)
-        return dfs(0, 0)
+        return math.comb(m+n-2, m-1)
+        
