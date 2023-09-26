1cb296a2b623f45546c5712fbdb5bf36127e50d1
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-09-26 10:34:35 +0530

    Time: 93 ms (5.18%) | Memory: 32.8 MB (7.99%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -3,8 +3,6 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         list1=list(s.replace(" ",""))
         list2=list(t.replace(" ",""))
-        print(list1)
-        print(list2)
         res=[ele for ele in list2]
         for i in list1:
             if(i in list2):
