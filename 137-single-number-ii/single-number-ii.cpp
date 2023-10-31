26d35590fe78e0fc807c6c12a477d7fe1e954201
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-11-01 02:22:58 +0530

    Time: 605 ms (5.18%) | Memory: 9.7 MB (73.95%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            int count=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+            if(count==1){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+        
+};
