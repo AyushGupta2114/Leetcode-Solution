2644460e392d650f23762c4dd38f4162295414fd
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-12-17 15:11:17 +0530

    Time: 115 ms (94.65%) | Memory: 19.5 MB (41.50%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -1,19 +1,27 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        k=[str(i) for i in s]
-        # print(k)
-        a=['a','e','i','o','u','A','E','I','O','U']
-        b=[]
-        for i in s:
-            if i in a:
-                b.append(i)
-        b.sort()
-        for i in range(0,len(k)):
-            if k[i] in a:
-                # print(a[i],b[0])
-                k[i]=b[0]
-                b.remove(b[0])
-        return ''.join(k)
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+        sorted_vowels = iter(sorted(char for char in s if char in vowels))
+
+        result = [next(sorted_vowels) if char in vowels else char for char in s]
+
+        return ''.join(result)
+# class Solution:
+#     def sortVowels(self, s: str) -> str:
+#         k=[str(i) for i in s]
+#         # print(k)
+#         a=['a','e','i','o','u','A','E','I','O','U']
+#         b=[]
+#         for i in s:
+#             if i in a:
+#                 b.append(i)
+#         b.sort()
+#         for i in range(0,len(k)):
+#             if k[i] in a:
+#                 # print(a[i],b[0])
+#                 k[i]=b[0]
+#                 b.remove(b[0])
+#         return ''.join(k)
 
 
 
