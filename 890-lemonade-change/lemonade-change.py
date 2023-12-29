3d04d35cd12fd618f43f33b469831955bcc395dd
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2023-12-30 02:58:35 +0530

    Time: 638 ms (94.43%) | Memory: 22 MB (7.49%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,58 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        num_of_10 = 0
+        num_of_5 = 0
+        # num_of_20 = 0
+        shopper = 0
+        for i in bills:
+            if i == 5:
+                num_of_5 += 1
+                shopper += i
+            elif i == 10:
+                num_of_10 += 1
+                shopper += i
+                shopper-=5
+                if(num_of_5>=1):
+                    num_of_5-=1
+                    continue
+                else:
+                    return False
+            else:
+                shopper+=20
+                if(num_of_10>=1 and num_of_5>=1):
+                    num_of_10-=1
+                    num_of_5-=1
+                elif(num_of_5>=3):
+                    num_of_5-=3
+                else:
+                    return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # shopper=0
+        # for i in bills:
+        #     if(i==5):
+        #         shopper+=i
+        #         print(shopper)
+        #     elif(i>5):
+        #         shopper+=i
+        #         shopper-=5
+        #         print(shopper)
+        #         if(shopper<0):
+        #             return False
+        # return True
