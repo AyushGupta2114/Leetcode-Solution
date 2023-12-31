4ea8f343ee7a212450b9aa4dc89f2f157b0b57ce
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-01 01:53:40 +0530

    Time: 93 ms (5.08%) | Memory: 17.3 MB (12.59%) - LeetSync

---
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+
+# Dictionary to store the first and last appearance of each character
+        appearances = {}
+
+# Variable to store the maximum distance
+        max1 = -1
+
+        for i, char in enumerate(s):
+            if char not in appearances:
+                # If the character is not in the dictionary, store its first appearance
+                appearances[char] = {"first": i, "last": i}
+            else:
+                # If the character is already in the dictionary, update its last appearance
+                appearances[char]["last"] = i
+                # Update the maximum distance if needed
+                max1 = max(max1, appearances[char]["last"] - appearances[char]["first"])
+
+        # Print the maximum distance
+        print("Maximum distance between first and last appearances:", max1)
+        if(max1==-1):
+            return -1
+        return max1-1
+        # old method 
+        # max=-1
+        # a=[]
+        # for i in range(0,len(s)):
+        #     if s[i] not in a:
+        #         a.append(s[i])
+        #     else:
+        #         fi = s.index(s[i])
+        #         se = s.index(s[i], fi + 1)
+
+        #         if(se-fi)>max:
+        #             max=se-fi
+        # if(max==-1):
+        #     return -1
+        # return max-1
+
+        
