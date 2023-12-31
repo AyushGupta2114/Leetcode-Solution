825bd11ebc85fe66ac74aaf2e53f4b35ed262f7f
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-01-01 02:22:53 +0530

    Time: 39 ms (65.62%) | Memory: 17.2 MB (12.59%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
 
@@ -37,5 +38,38 @@ class Solution:
         # if(max==-1):
         #     return -1
         # return max-1
+
+        # class Solution:
+#     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
+
+#         # appearances = {}
+#         # max1 = -1
+#         # for i, char in enumerate(s):
+#         #     if char not in appearances:
+#         #         appearances[char] = {"first": i, "last": i}
+#         #     else:
+#         #         appearances[char]["last"] = i
+#         #         max1 = max(max1, appearances[char]["last"] - appearances[char]["first"])
+
+#         # print("Maximum distance between first and last appearances:", max1)
+#         # if(max1==-1):
+#         #     return -1
+#         # return max1-1
+#         # old method 
+#         max1=-1
+#         a=[]
+#         for i in range(0,len(s)):
+#             if s[i] not in a:
+#                 a.append(s[i])
+#             else:
+#                 fi = s.index(s[i])
+#                 se = s.index(s[i], fi + 1)
+
+#                 if(se-fi)>max1:
+#                     max1=se-fi
+#                 print(se,fi)
+#         if(max1==-1):
+#             return -1
+#         return max1-1
 
         
