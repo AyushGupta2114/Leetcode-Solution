964684c773a9e985b5770a5886366b9c01cc974f
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 01:00:00 +0530

    Time: 54 ms (26.99%) | Memory: 16.5 MB (52.29%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -15,8 +15,8 @@ class Solution:
         # print(i)
 
         for i in range(1,n+1):
-            a=int((i*(i+1))/2)
-            b=int(((n-i+1)/2)*(n+i))
+            a=(i*(i+1))/2
+            b=((n-i+1)/2)*(n+i)
             # print(a,b)
             if a==b:
                 return i
