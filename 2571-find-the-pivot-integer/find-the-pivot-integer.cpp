31d61c9c9f23e17b6aed074aefb8b72828ece7f2
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 00:47:13 +0530

    Time: 19 ms (9.22%) | Memory: 7.1 MB (82.41%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int s1=0,s2=0,pivot=0;
+        for(int i=1;i<=n;i++)
+        {
+            s1+=i;
+            for(int j=i;j<=n;j++){
+                s2+=j;
+                if(s2<s1){
+                    continue;
+                }
+            }
+            if(s1==s2){
+            pivot=i;
+            break;
+            }
+            s2=0;
+        }
+        if(pivot==0)
+        return -1;
+        else
+        return pivot;
+    }
+};
