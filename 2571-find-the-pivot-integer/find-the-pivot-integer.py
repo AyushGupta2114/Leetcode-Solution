27de77fbaaa6c97856c35065d33c681e554d5b95
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 14:55:46 +0530

    Time: 42 ms (56.60%) | Memory: 16.6 MB (52.32%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,27 +1,8 @@
-class Solution:
-    def pivotInteger(self, n: int) -> int:
-        # a=0
-        # b=0
-        # i=1
-        # x=True
-        # while(x):
-        #     a+=n
-        #     n-=1
-        #     b+=i
-        #     i+=1
-        #     print(a,b)
-        #     if(a==b):
-        #         x=False
-        # print(i)
-
-        for i in range(1,n+1):
-            a=(i*(i+1))/2
-            b=((n-i+1)/2)*(n+i)
-            # print(a,b)
-            if a==b:
-                return i
-        return -1
-            
-
-
+class Solution(object):
+    def pivotInteger(self, n):
+        x = sqrt(n * (n + 1) / 2)
         
+        if x % 1 != 0:
+            return -1
+        else:
+            return int(x)
