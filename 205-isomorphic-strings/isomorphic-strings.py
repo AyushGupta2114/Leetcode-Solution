778c4ed4e5b5316f61de3ee6ebe507f4af976acc
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-03-21 14:23:27 +0530

    Time: 44 ms (48.84%) | Memory: 16.9 MB (32.47%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        x=Counter(s)
+        y=Counter(t)
+        if(list(x.values())!=list(y.values())):
+            print(list(x.values()),list(y.values()))
+            return False
+        a={}
+        for i in range(0,len(s)):
+            if(s[i] in a):
+                value=a.get(t[i])
+                if (a[s[i]]!=t[i]):
+                    return False
+                    
+            else:
+                a[s[i]]=t[i]
+        print(a)
+        return True
+
+        
