6ca7f69ac4e0c2dde3642cfaf3d44429cb9dbe9a
Ayush Gupta <94870982+AyushGupta2114@users.noreply.github.com>
2024-06-12 02:37:00 +0530

    Time: 46 ms (27.82%) | Memory: 16.6 MB (93.33%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,19 +1,17 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        a=[]
         b=[]
         for i in arr2:
             if i not in arr1:
-                a.append(i)
                 arr1.remove(i)
-        print(a)
+        # print(a)
         arr1.sort()
         for i in arr2:
             while(i in arr1):
                 b.append(i)
                 arr1.remove(i)
-        print(b)
-        print(arr1)
+        # print(b)
+        # print(arr1)
         return b+arr1
 
 
